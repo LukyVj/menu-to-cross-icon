84cea5f6d7b62d777ba395c86c014fc954c568ab
Lucas Bonomi <lcuas.bonomi@gmail.com>
2015-01-19 17:46:44 +0100

    First push

---
+++ scripts/getCss.js
@@ -0,0 +1,31 @@
+function css(a) {
+    var sheets = document.styleSheets, o = {};
+    for (var i in sheets) {
+        var rules = sheets[i].rules || sheets[i].cssRules;
+        for (var r in rules) {
+            if (a.is(rules[r].selectorText)) {
+                o = $.extend(o, css2json(rules[r].style), css2json(a.attr('style')));
+            }
+        }
+    }
+    return o;
+}
+
+function css2json(css) {
+    var s = {};
+    if (!css) return s;
+    if (css instanceof CSSStyleDeclaration) {
+        for (var i in css) {
+            if ((css[i]).toLowerCase) {
+                s[(css[i]).toLowerCase()] = (css[css[i]]);
+            }
+        }
+    } else if (typeof css == "string") {
+        css = css.split("; ");
+        for (var i in css) {
+            var l = css[i].split(": ");
+            s[l[0].toLowerCase()] = (l[1]);
+        }
+    }
+    return s;
+}
---
+++ scripts/main.js
@@ -0,0 +1,144 @@
+/* 
+Menu to cross icon - 2015
+Author : @LukyVj 
+http://lucasbonomi.com 
+*/
+// Unrelated to icons 
+$('.howmuch').empty().append($('section').length);
+
+
+$(document).keydown(function(e) {
+  switch(e.which) {
+
+    case 38: // up
+      $('html:not(:animated), body:not(:animated)').animate({
+        scrollTop: $(window).scrollTop()  - $(window).height()
+      });
+
+    case 40: // down
+      $('html:not(:animated), body:not(:animated)').animate({
+        scrollTop: $(window).scrollTop() + $(window).height()
+      });
+
+
+    default: return; // exit this handler for other keys
+  }
+  e.preventDefault(); // prevent the default action (scroll / move caret)
+});
+
+$('.show_snippet').on('click', function(e){
+  e.preventDefault();
+  var $this = $(this);
+  $this.parent().parent().find('.snippet').toggleClass('show');
+  $this.parent().toggleClass('ckd');
+});
+$('.codepen-able').each( function(e){
+ 
+
+    var el = $(this);
+        type = el.parent().parent().find('pre').data("type"),
+        codeInside = el.parent().parent().find('pre').find("code"),
+        isCodeInside = codeInside.length,
+        HTML = "",
+        CSS = "",
+        JS = "";
+        
+       isHTML = el.parent().parent().find('pre[data-type="html"] code'),
+       isCSS = el.parent().parent().find('pre[data-type="css"] code'),
+       isJS = el.parent().parent().find('pre[data-type="js"] code'),
+
+    
+
+       HTML = isHTML.html();
+       CSS = isCSS.html();
+       JS = isJS.html();
+
+    var data = {
+      title              : "",
+      description        : "",
+      html               : HTML,
+      html_pre_processor : "none",
+      css                : CSS,
+      css_pre_processor  : "none",
+      css_starter        : "neither",
+      css_prefix_free    : false,
+      js                 : JS,
+      js_pre_processor   : "none",
+      js_modernizr       : false,
+      js_library         : "jquery",
+      html_classes       : "",
+      css_external       : "",
+      js_external        : ""
+    };
+
+    var JSONstring = 
+      JSON.stringify(data)
+      // Quotes will screw up the JSON
+      .replace(/"/g, "&quot;")
+      .replace(/'/g, "&apos;");
+
+    var form = 
+      '<form action="http://codepen.io/pen/define" method="POST" target="_blank">' + 
+        '<input type="hidden" name="data" value=\'' + 
+          JSONstring + 
+          '\'>' + 
+        '<input type="image" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/64/Shape.svg" width="40" height="40" value="Create New Pen with Prefilled Data" class="codepen-mover-button">' +
+      '</form>';
+
+    el.append(form);
+
+  });
+  
+
+
+// Sample 2 
+$(document).ready(function(){
+
+  var svg = $('svg');
+  var lines = svg.find('rect');
+  var line_first = $('svg rect:nth-child(1)')
+  var line_second = $('svg rect:nth-child(2)')
+  var line_third = $('svg rect:nth-child(3)')
+  var i = true;
+
+  svg.on('click', function(){
+    if (i) {
+      setTimeout(function(){
+        $(this).find("g").addClass('gotcha')
+
+        line_first.css({
+          'transform':'rotate(45deg)',
+          'left':'50%',
+          'top':'50%',
+          'width':'200px',
+          // This line BELONGS to @dervondenbergen :D 
+          // Enjoy your propriety my friend.
+          'transform-origin': 'left bottom'
+        })
+        line_third.css({
+          'transform':'rotate(-45deg) translate(-50%,-50%)',
+          'left':'50%',
+          'top':'50%'
+        })
+        line_second.css('opacity','0')
+      },005)
+    } else {
+      setTimeout(function(){
+        line_first.attr('style', '');
+        line_third.attr('style', '');
+        line_second.css('opacity','1')
+      },005)
+    }
+    i=!i; 
+  })
+
+
+  // Sample 4 
+  var $block = $('.sample--4'),
+      $icon = $block.find('span');
+  $icon.on('click',function(e){
+    $(this).toggleClass('close')
+  });
+
+});
+
